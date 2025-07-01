c463d7c42dd0bd3a7d937f9c4d8c590a55fca0b9
Maher Hamdi <maherhamdii7@gmail.com>
2025-07-01 19:17:44 +0100

    FinalVersion 1.0

--- client/src/services/toDoServices.js
+++ client/src/services/toDoServices.js
@@ -3,29 +3,27 @@ import { getUserDetails } from '../util/GetUser';
 
 const SERVER_URL = 'http://localhost:5000/api/todo';
 
-const authHeader = () => {
-    let userToken = getUserDetails()?.token;
-    return {headers:{'Authorization': userToken}}
+const authHeaders = ()=>{
+    let  userToken = getUserDetails()?.token;
+    return {headers:{'Authorization':userToken}}
+
 }
 
-const createToDo = (data) => {
-    return axios.post(SERVER_URL+'/create-to-do',data,authHeader());
-    
-}
-const getAllToDo = (userId) => {
-    return axios.get(SERVER_URL+'/get-all-to-do/'+userId,authHeader());
-    
-}
-const deleteToDo = (id) => {
-    return axios.delete(SERVER_URL+'/delete-to-do'+id,authHeader());
-    
-}
-const updateToDo = (id,data) => {
-    return axios.patch(SERVER_URL+'/update-to-do'+id,data,authHeader());
-    
+const createToDo = (data)=>{
+    return axios.post(SERVER_URL+'/create-to-do',data,authHeaders());
 }
 
+const getAllToDo = (userId)=>{
+    return axios.get(SERVER_URL+'/get-all-to-do/'+userId,authHeaders());
+}
 
+const deleteToDo = (id)=>{
+    return axios.delete(SERVER_URL+'/delete-to-do/'+id,authHeaders());
+}
+
+const updateToDo = (id,data)=>{
+    return axios.patch(SERVER_URL+'/update-to-do/'+id,data,authHeaders());
+}
 
 
 
@@ -34,7 +32,9 @@ const ToDoServices = {
     getAllToDo,
     deleteToDo,
     updateToDo
-    
 }
 
+
 export default ToDoServices;
+
+
